70e95590fcb4fe4ccd2a7c844e065d31689ccd46
poramet-bit <poramet-bit>
2026-01-23 16:49:07 +0700

    แก้ตัว login

--- Fronend_v1/login_project/action123.js
+++ Fronend_v1/login_project/action123.js
@@ -1,4 +1,5 @@
-function goToStep(stepNumber) {
+/*-------------  Create Student---*/
+function goToStep_stu(stepNumber) {
     // 1. ซ่อนทุกส่วนก่อน
     document.getElementById('part1').style.display = 'none';
     document.getElementById('part2').style.display = 'none';
@@ -20,18 +21,41 @@ function goToStep(stepNumber) {
     }
 }
 
-function goBack() {
+function goBack_stu() {
     // หาว่าตอนนี้หน้าไหนกำลังแสดงอยู่ (ดูจาก display ที่ไม่ใช่ none)
     if (document.getElementById('part2').style.display === 'block') {
-        goToStep(1);
+        goToStep_stu(1);
         // ถอดสีม่วงออกจากวงกลม 2 และเส้น 1
         document.getElementById('circle2').classList.remove('active');
         document.getElementById('line1').classList.remove('active');
     } 
     else if (document.getElementById('part3').style.display === 'block') {
-        goToStep(2);
+        goToStep_stu(2);
         // ถอดสีม่วงออกจากวงกลม 3 และเส้น 2
         document.getElementById('circle3').classList.remove('active');
         document.getElementById('line2').classList.remove('active');
+    }
+}
+ /*--------------  Create Genneral -----------------*/
+
+ function goToStep_gen(stepNumber) {
+
+    document.getElementById('p1').style.display = 'none';
+    document.getElementById('p2').style.display = 'none';
+
+    document.getElementById('p' + stepNumber).style.display = 'block';
+
+    if (stepNumber === 2) {
+        document.getElementById('c2').classList.add('active');
+        document.getElementById('l1').classList.add('active');
+    }
+}
+function goBack_gen() {
+    // หาว่าตอนนี้หน้าไหนกำลังแสดงอยู่ (ดูจาก display ที่ไม่ใช่ none)
+    if (document.getElementById('p2').style.display === 'block') {
+        goToStep_gen(1);
+        // ถอดสีม่วงออกจากวงกลม 2 และเส้น 1
+        document.getElementById('c2').classList.remove('active');
+        document.getElementById('l1').classList.remove('active');
     }
 }
